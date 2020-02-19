157db0245ee233639cbf3a5e45c5f65c7a10d835
VladimirBogomolov96 <vladimir.bogomolov96@gmail.com>
2020-02-19 19:39:14 +0300

    Add storage command line parameter.

---
+++ FileCabinetApp/Options.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace FileCabinetApp
+{
+    /// <summary>
+    /// Command line options.
+    /// </summary>
+    public class Options
+    {
+        /// <summary>
+        /// Gets or sets type of validation rules.
+        /// </summary>
+        /// <value>Type of validation rules.</value>
+        [Option('v', "validation-rules", Default = "default", Required = false)]
+        public string Rule { get; set; }
+
+        /// <summary>
+        /// Gets or sets type of storage.
+        /// </summary>
+        /// <value>Type of storage.</value>
+        [Option('s', "storage", Default = "memory", Required = false)]
+        public string Storage { get; set; }
+    }
+}
