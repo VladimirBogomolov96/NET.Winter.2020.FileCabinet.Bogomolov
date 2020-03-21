6a3b671f48555ff1dd48ee6048a3a0f72b587994
VladimirBogomolov96 <vladimir.bogomolov96@gmail.com>
2020-03-21 17:27:01 +0300

    Add hints to similar commands.

--- FileCabinetApp/CommandHandlers/CommandHandlerBase.cs
+++ FileCabinetApp/CommandHandlers/CommandHandlerBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace FileCabinetApp.CommandHandlers
@@ -9,6 +10,22 @@ namespace FileCabinetApp.CommandHandlers
     /// </summary>
     public abstract class CommandHandlerBase : ICommandHandler
     {
+        private readonly string[] commands = new string[]
+        {
+            "help",
+            "exit",
+            "stat",
+            "create",
+            "list",
+            "find",
+            "export",
+            "import",
+            "purge",
+            "insert",
+            "delete",
+            "update",
+        };
+
         private ICommandHandler commandHandler;
 
         /// <summary>
@@ -28,7 +45,25 @@ namespace FileCabinetApp.CommandHandlers
             }
             else
             {
-                Console.WriteLine($"Command {commandRequest.Command} doesn't exit.");
+                IEnumerable<string> similarCommands = this.GetSimilarCommands(commandRequest.Command);
+                if (!similarCommands.Any())
+                {
+                    Console.WriteLine($"Command {commandRequest.Command} doesn't exist.");
+                }
+                else if (similarCommands.Count() == 1)
+                {
+                    Console.WriteLine($"Command {commandRequest.Command} doesn't exist.");
+                    Console.WriteLine($"The most similar command is '{similarCommands.First()}'");
+                }
+                else
+                {
+                    Console.WriteLine($"Command {commandRequest.Command} doesn't exist.");
+                    Console.WriteLine("The most similar commands are:");
+                    foreach (string command in similarCommands)
+                    {
+                        Console.WriteLine(command);
+                    }
+                }
             }
         }
 
@@ -41,6 +76,63 @@ namespace FileCabinetApp.CommandHandlers
         {
             this.commandHandler = commandHandler;
             return this.commandHandler;
+        }
+
+        private IEnumerable<string> GetSimilarCommands(string input)
+        {
+            foreach (string command in this.commands)
+            {
+                if (this.LevensteinAlgorithm(input, command) < 4)
+                {
+                    yield return command;
+                }
+            }
+        }
+
+        private int LevensteinAlgorithm(string input, string command)
+        {
+            int n = input.Length;
+            int m = command.Length;
+            int[][] matrix = new int[n + 1][];
+
+            for (int i = 0; i < n + 1; i++)
+            {
+                matrix[i] = new int[m + 1];
+            }
+
+            if (n == 0)
+            {
+                return m;
+            }
+
+            if (m == 0)
+            {
+                return n;
+            }
+
+            for (int i = 0; i <= n; i++)
+            {
+                matrix[i][0] = i;
+            }
+
+            for (int j = 0; j <= m; j++)
+            {
+                matrix[0][j] = j;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    int cost = (command[j - 1] == input[i - 1]) ? 0 : 1;
+
+                    matrix[i][j] = Math.Min(
+                        Math.Min(matrix[i - 1][j] + 1, matrix[i][j - 1] + 1),
+                        matrix[i - 1][j - 1] + cost);
+                }
+            }
+
+            return matrix[n][m];
         }
     }
 }
